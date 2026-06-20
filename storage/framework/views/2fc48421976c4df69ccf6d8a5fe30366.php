477552e1bcd420e2795d0658a8ffe2e55c2ba478
NASASIRA KENETH <kendricjael@gmail.com>
2026-06-20 17:48:38 +0300

    Remove generated cache/log files from version control

--- storage/framework/views/2fc48421976c4df69ccf6d8a5fe30366.php
+++
@@ -1,5 +0,0 @@
-<button <?php echo e($attributes->merge(['type' => 'submit', 'class' => 'btn btn-primary'])); ?>>
-    <?php echo e($slot); ?>
-
-</button>
-<?php /**PATH C:\Users\future\Desktop\hospital-management-system\hospital-management-system\resources\views/components/primary-button.blade.php ENDPATH**/ ?>
